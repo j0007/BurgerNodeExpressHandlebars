47eb6f666c46f6baeb47a1f68906fdf5a1a7da37
Jchn <jchn.wrk@gmail.com>
2019-03-17 04:35:41 -0400

    update

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheese burger');
+INSERT INTO burgers (burger_name) VALUES ('Tofurger');
+INSERT INTO burgers (burger_name) VALUES ('Beef burger');
+INSERT INTO burgers (burger_name) VALUES ('Veggie burger');
+
+Select * from burgers;
